1ce76eb54b35c5f217ba0141555bc76af3c968a6
tangrisheng <tangrisheng@zhiwen.me>
2019-05-10 18:50:14 +0800

    update

--- build.sh
+++ build.sh
@@ -5,23 +5,22 @@
 # 登录服务器并且删除文件夹
 set +e
 ssh -o StrictHostKeyChecking=no ec2-user@52.82.65.180 << eeooff
-if [[  -d ~/express-example ]]; then
+if [[  -d ~/express-example-staging ]]; then
     # 文件夹存在
-    cd ~/express-example
+    cd ~/express-example-staging
     pm2 stop pm2.json --env dev
-    rm -rf ~/express-example
+    rm -rf ~/express-example-staging
 fi
-mkdir -p ~/express-example
+mkdir -p ~/express-example-staging
 exit
 eeooff
 
 # 拷贝文件到服务器
-serverPath = "ec2-user@52.82.65.180:" + path
-scp -r ./* "$serverPath"
+scp -r ./* ec2-user@52.82.65.180:~/express-example-staging
 
 # 运行
 ssh -o StrictHostKeyChecking=no ec2-user@52.82.65.180 '"$path" $path'<< eeooff
-cd ~/express-example
+cd ~/express-example-staging
 npm install
 pm2 startOrRestart pm2.json --env dev
 
