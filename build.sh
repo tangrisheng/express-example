a1c735d3e3dbbf8cc7d5ccab21236bca0f90b80f
tangrisheng <tangrisheng@zhiwen.me>
2019-05-13 10:45:57 +0800

    update webhook

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@
 
 # 登录服务器并且删除文件夹
 set +e
-ssh -o StrictHostKeyChecking=no ec2-user@52.82.65.180 << eeooff
-if [[  -d ~/express-example-staging ]]; then
+ssh -o StrictHostKeyChecking=no zhiwen-server << eeooff
+if [  -d ~/express-example-staging ]; then
     # 文件夹存在
+    echo "---------folder exist---------"
     cd ~/express-example-staging
-    pm2 stop pm2.json --env dev
+    pm2 del pm2.json --env dev
     rm -rf ~/express-example-staging
 fi
 mkdir -p ~/express-example-staging
@@ -16,10 +17,11 @@ exit
 eeooff
 
 # 拷贝文件到服务器
-scp -r ./* ec2-user@52.82.65.180:~/express-example-staging
+echo "copy files"
+scp -r ./* zhiwen-server:~/express-example-staging
 
 # 运行
-ssh -o StrictHostKeyChecking=no ec2-user@52.82.65.180 '"$path" $path'<< eeooff
+ssh -o StrictHostKeyChecking=no zhiwen-server << eeooff
 cd ~/express-example-staging
 npm install
 pm2 startOrRestart pm2.json --env dev
